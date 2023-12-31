9575c14c5537639448c3c55756508c16441c06b0
Benjamin <Benjamin@DESKTOP-7BCRGI1>
2023-12-30 23:28:04 -0800

    Add project files.

---
+++ MIS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MIS // Note: actual namespace depends on the project name.
+{
+    internal class MISRUNNER
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
